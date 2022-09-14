dad58ef7c087189b09202711143df4d221957d76
rodel <rodel.calasagsag@automattic.com>
2022-09-15 01:48:25 +0800

    Paginate

--- .github/workflows/get-pr-list.sh
+++ .github/workflows/get-pr-list.sh
@@ -2,8 +2,10 @@
 
 echo '| PR | Include | To be tested by | Notes |'
 echo '| -- | ------- | --------------- | ----- |'
+
 gh api repos/woocommerce/woocommerce/pulls \
     --method GET \
     -F milestone=6.9.0 \
+    --paginate \
     --template \
     '{{range .}}| [{{.title}}]({{.url}}) | | | |{{"\n"}}{{end}}'
