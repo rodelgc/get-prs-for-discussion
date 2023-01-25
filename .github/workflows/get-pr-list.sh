05d50597f19d3a6a80069cd463129947d435431a
rodelgc <rodel.calasagsag@automattic.com>
2023-01-25 15:18:29 +0800

    Update get-pr-list.sh

--- .github/workflows/get-pr-list.sh
+++ .github/workflows/get-pr-list.sh
@@ -3,7 +3,7 @@
 echo '| PR | Include | To be tested by | Notes |'
 echo '| -- | ------- | --------------- | ----- |'
 
-gh search prs -L 200 \
+gh search prs -L 1000 \
     --repo woocommerce/woocommerce \
     --milestone $MILESTONE \
     --json 'title,url' \
